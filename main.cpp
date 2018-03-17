e5e5c166a45466de4e859e086ab1f3db20501e51
liang <pwrliang@gmail.com>
2018-03-17 17:55:35 -0400

    refine code

--- main.cpp
+++ main.cpp
@@ -15,24 +15,39 @@ int main(int argc, char **argv) {
     gflags::ParseCommandLineFlags(&argc, &argv, true);
 
     int log_numverts = FLAGS_scale;
-    int nverts = 1 << log_numverts;
-    int64_t desired_nedges = FLAGS_edge_factor * nverts;
+    int64_t nverts = (int64_t) 1 << log_numverts;
+    int64_t desired_nedges = FLAGS_edge_factor * (int64_t) nverts;
+    int64_t max_edges = nverts * (nverts - 1) / 2;
     int64_t nedges;
     packed_edge *result;
 
-    FILE *fp = fopen(FLAGS_output.data(), "w");
-    if (fp == nullptr) {
-        printf("CAN NOT OPEN %s\n", FLAGS_output.data());
-        exit(1);
+
+    if (nverts >= UINT32_MAX) {
+        printf("the number of vertices can not more than:%u\n", UINT32_MAX);
+        return 1;
     }
 
+    if (desired_nedges > max_edges) {
+        printf("vertices: %lld desired edges: %lld, but edges can not more than: %lld\n",
+               (long long) nverts,
+               (long long) desired_nedges,
+               (long long) max_edges);
+        return 1;
+    }
+
+
+    FILE *fp = fopen(FLAGS_output.data(), "w");
+    if (fp == nullptr) {
+        printf("CAN NOT OPEN FILE %s\n", FLAGS_output.data());
+        exit(1);
+    }
 
     printf("graph info: scale:%d edge_factor:%d %s output:%s\n",
            FLAGS_scale, FLAGS_edge_factor, FLAGS_directed ? "directed" : "undirected", FLAGS_output.data());
 
     make_graph(log_numverts, desired_nedges, 1, 2, &nedges, &result);
 
-    std::set<std::pair<int64_t, int64_t>> edges;
+    std::set<std::pair<uint32_t, uint32_t>> edges;
 
     for (int64_t k = 0; k < nedges; ++k) {
         const int64_t i = get_v0_from_edge(&result[k]);
@@ -43,11 +58,11 @@ int main(int argc, char **argv) {
             edges.insert({j, i});
     }
 
-    printf("num_verts:%d, desired_edges:%lld num_edges:%lu\n", nverts, (long long) desired_nedges, edges.size());
+    printf("num_verts:%u, desired_edges:%lld num_edges:%lu\n", nverts, (long long) desired_nedges, edges.size());
     printf("writing...\n");
 
     for (const auto &edge : edges) {
-        fprintf(fp, "%lld %lld\n", (long long) edge.first, (long long) edge.second);
+        fprintf(fp, "%u %u\n", edge.first, edge.second);
     }
 
     free(result);
